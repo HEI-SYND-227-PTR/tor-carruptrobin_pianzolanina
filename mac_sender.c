3781258835e2273bd716aa87fe70eb6c3032f153
Robin Carrupt <43844979+Robibobo1@users.noreply.github.com>
2024-04-17 15:37:26 +0200

    DataInd

--- mac_sender.c
+++ mac_sender.c
@@ -7,38 +7,55 @@
 void sendTestMessage()
 {
 	struct queueMsg_t queueMsg;		// queue message
-	uint8_t * msg;
-	char testMessage[] = "Hello World";
-	uint8_t destAddr = 10;
-	uint8_t destSAPI = CHAT_SAPI;
-	size_t	length = sizeof(testMessage) - 1;
-	bool read  = true;
-	bool ack = true;
 	osStatus_t retCode;
 	
 	//------------------------------------------------------------------------------
 	for (;;)											// loop until doomsday
 	{
-		//----------------------------------------------------------------------------
-		// MEMORY ALLOCATION				
-		//----------------------------------------------------------------------------
-		msg = osMemoryPoolAlloc(memPool,osWaitForever);											
-	
-		msg[0] = gTokenInterface.myAddress << 3 + gTokenInterface.currentView; // Control 1
-		msg[1] = destAddr << 3 + destSAPI;
-		msg[2] = length;
-		memcpy(&msg[3],testMessage,length);
-		uint8_t crc = msg[0] + msg[1] + msg[2];
-		for(uint8_t i = 0; i<length; i++)
+		retCode = osMessageQueueGet( 	
+			queue_chatR_id,
+			&queueMsg,
+			NULL,
+			osWaitForever); 	
+    CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);		
+		
+		switch(queueMsg.type)
 		{
-			crc = crc + msg[3 + i];
+			case TOKEN:
+				break;
+			case DATABACK:
+				break;
+			case NEW_TOKEN:
+				break;
+			case START:
+				break;
+			case STOP:
+				break;
+			case DATA_IND:
+				
+				uint8_t* msg = osMemoryPoolAlloc(memPool,osWaitForever);											
+				msg[0] = gTokenInterface.myAddress << 3 + queueMsg.sapi; // Control 1
+				msg[1] = queueMsg.addr << 3 + queueMsg.sapi;
+				size_t length = strlen(queueMsg.anyPtr);
+				msg[2] = length;
+				memcpy(&msg[3],queueMsg.anyPtr,length);
+				uint8_t crc = msg[0] + msg[1] + msg[2];
+				for(uint8_t i = 0; i<length; i++){
+					crc = crc + msg[3 + i];
+				}
+				bool read  = false;
+				bool ack = false;
+				uint8_t status = (crc << 2) + (read << 1) + ack;
+				msg[3 + length] = status;
+				
+				queueMsg.type = TO_PHY;
+				queueMsg.anyPtr = msg;
+		
+				break;
 		}
-		uint8_t status = (crc << 2) + (read << 1) + ack;
-		msg[3 + length] = status;
 		
-		queueMsg.type = TO_PHY;
-		queueMsg.anyPtr = msg;
-		
+
+
 		//--------------------------------------------------------------------------
 		// QUEUE SEND	(send received frame to physical layer sender)
 		//--------------------------------------------------------------------------
