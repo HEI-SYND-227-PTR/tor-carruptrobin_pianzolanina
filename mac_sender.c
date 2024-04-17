b032324ff06e26ed9d9fbbbb6d52f3f9a9233094
ninapianzola1 <160869640+ninapianzola1@users.noreply.github.com>
2024-04-17 16:06:14 +0200

    Update mac_sender.c
    Databack try

--- mac_sender.c
+++ mac_sender.c
@@ -22,8 +22,33 @@ void sendTestMessage()
 		switch(queueMsg.type)
 		{
 			case TOKEN:
+			{
+				
+			}
 				break;
 			case DATABACK:
+			{
+				uint8_t* msg = osMemoryPoolAlloc(memPool,osWaitForever);
+				msg[0] = (queueMsg.addr << 3) + queueMsg.sapi;
+				msg[1] = queueMsg.anyPtr+1;
+				size_t length = strlen(queueMsg.anyPtr);
+				msg[2] = length;
+				memcpy(&msg[3],queueMsg.anyPtr,length);
+				uint8_t crc = msg[0] + msg[1] + msg[2];
+				for(uint8_t i = 0; i<length; i++){
+					crc = crc + msg[3 + i];
+				}
+				bool read  = true;
+				bool ack = false;
+				if(((queueMsg.anyPtr+1) >> 3 == 0xFF) || (crc == (queueMsg.anyPtr+length >> 2)){
+					bool ack = true;
+				}
+				uint8_t status = (crc << 2) + (read << 1) + ack;
+				msg[3 + length] = status;
+				
+				queueMsg.type = TO_PHY;
+				queueMsg.anyPtr = msg;
+			}
 				break;
 			case NEW_TOKEN:
 				break;
@@ -32,7 +57,7 @@ void sendTestMessage()
 			case STOP:
 				break;
 			case DATA_IND:
-				
+			{
 				uint8_t* msg = osMemoryPoolAlloc(memPool,osWaitForever);											
 				msg[0] = gTokenInterface.myAddress << 3 + queueMsg.sapi; // Control 1
 				msg[1] = queueMsg.addr << 3 + queueMsg.sapi;
@@ -50,7 +75,7 @@ void sendTestMessage()
 				
 				queueMsg.type = TO_PHY;
 				queueMsg.anyPtr = msg;
-		
+			}
 				break;
 		}
 		
