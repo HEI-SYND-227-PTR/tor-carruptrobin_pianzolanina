7ca1789363742b13451309b0dcec9dabc71ccc23
Robin Carrupt <43844979+Robibobo1@users.noreply.github.com>
2024-04-22 13:04:37 +0200

    Updated Token logic

--- mac_sender.c
+++ mac_sender.c
@@ -25,88 +25,94 @@ void MacSender(void *argument)
 			osWaitForever);
 		CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);
 
-		//if (queueMsg.type == DATABACK) //Todo next
+		if (queueMsg.type == DATABACK && iHavetheToken == true) // Todo next
+		{
+			uint8_t *dataPtr = (uint8_t *)queueMsg.anyPtr;
+			bool read = (dataPtr[3 + dataPtr[2]] & 2) >> 1;
+			bool ack = (dataPtr[3 + dataPtr[2]] & 1);
 
-		if (queueMsg.type == TOKEN)
+			if (read == 1 && ack == 1) // Message has made a full circle and is okay
+			{
+				retCode = osMemoryPoolFree(memPool, dataPtr);
+				CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);
+				queueMsg = tokenMsg;
+				iHavetheToken = false;
+			}
+			else
+			{
+				read = 0;
+				ack = 0;
+				dataPtr[3 + dataPtr[2]] = dataPtr[3 + dataPtr[2]] & (ack + (read << 1));
+				queueMsg.type = TO_PHY;
+				queueMsg.anyPtr = dataPtr;
+			}
+		}
+		else if (queueMsg.type == TOKEN)
 		{
 			iHavetheToken = true;
 			tokenMsg = queueMsg;
 
-			retCode = osMessageQueueGet(
-				queue_messBuff_id,
-				&queueMsg,
-				NULL,
-				osWaitForever);
-			CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);
+			if (osMessageQueueGetCount(queue_messBuff_id) != 0)
+			{
+				retCode = osMessageQueueGet(
+					queue_messBuff_id,
+					&queueMsg,
+					NULL,
+					osWaitForever);
+				CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);
 
-			switch (queueMsg.type)
-			{
-			case TOKEN:
-			{
-			}
-			break;
-			case DATABACK:
-			{
-				uint8_t *dataPtr = (uint8_t *)queueMsg.anyPtr;
-				bool read = (dataPtr[3 + dataPtr[2]] & 2) >> 1;
-				bool ack = (dataPtr[3 + dataPtr[2]] & 1);
-
-				if (read == 1 && ack == 1) // Message has made a full circle and is okay
+				switch (queueMsg.type)
 				{
-					retCode = osMemoryPoolFree(memPool, dataPtr);
-					CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);
-					queueMsg = tokenMsg;
-					iHavetheToken = false;
+				case NEW_TOKEN:
+				{
+					uint8_t *msg = osMemoryPoolAlloc(memPool, osWaitForever);
+					msg[0] = 0xFF;
+					for (uint8_t i = 0; i < 15; i++)
+					{
+						msg[1 + i] = gTokenInterface.station_list[i];
+					}
+					msg[16] = 0;
+					queueMsg.type = NEW_TOKEN;
+					queueMsg.addr = msg;
 				}
-				else
+				break;
+				case START:
 				{
-					read = 0;
-					ack = 0;
-					dataPtr[3 + dataPtr[2]] = dataPtr[3 + dataPtr[2]] & (ack + (read << 1));
+
+				}
+					break;
+				case STOP:
+				{
+					
+				}
+					break;
+				case DATA_IND:
+				{
+					uint8_t *msg = osMemoryPoolAlloc(memPool, osWaitForever);
+					msg[0] = gTokenInterface.myAddress << 3 + queueMsg.sapi; // Control 1
+					msg[1] = queueMsg.addr << 3 + queueMsg.sapi;
+					size_t length = strlen(queueMsg.anyPtr);
+					msg[2] = length;
+					memcpy(&msg[3], queueMsg.anyPtr, length);
+					uint8_t crc = msg[0] + msg[1] + msg[2];
+					for (uint8_t i = 0; i < length; i++)
+					{
+						crc = crc + msg[3 + i];
+					}
+					bool read = false;
+					bool ack = false;
+					uint8_t status = (crc << 2) + (read << 1) + ack;
+					msg[3 + length] = status;
+
 					queueMsg.type = TO_PHY;
-					queueMsg.anyPtr = dataPtr;
+					queueMsg.anyPtr = msg;
 				}
-			}
-			break;
-			case NEW_TOKEN:
-			{
-				uint8_t *msg = osMemoryPoolAlloc(memPool, osWaitForever);
-				msg[0] = 0xFF;
-				for (uint8_t i = 0; i < 15; i++)
-				{
-					msg[1 + i] = gTokenInterface.station_list[i];
-				}
-				msg[16] = 0;
-				queueMsg.type = NEW_TOKEN;
-				queueMsg.addr = msg;
-			}
-			break;
-			case START:
 				break;
-			case STOP:
-				break;
-			case DATA_IND:
-			{
-				uint8_t *msg = osMemoryPoolAlloc(memPool, osWaitForever);
-				msg[0] = gTokenInterface.myAddress << 3 + queueMsg.sapi; // Control 1
-				msg[1] = queueMsg.addr << 3 + queueMsg.sapi;
-				size_t length = strlen(queueMsg.anyPtr);
-				msg[2] = length;
-				memcpy(&msg[3], queueMsg.anyPtr, length);
-				uint8_t crc = msg[0] + msg[1] + msg[2];
-				for (uint8_t i = 0; i < length; i++)
-				{
-					crc = crc + msg[3 + i];
 				}
-				bool read = false;
-				bool ack = false;
-				uint8_t status = (crc << 2) + (read << 1) + ack;
-				msg[3 + length] = status;
-
-				queueMsg.type = TO_PHY;
-				queueMsg.anyPtr = msg;
 			}
-			break;
+			else
+			{
+				queueMsg = tokenMsg;
 			}
 
 			//--------------------------------------------------------------------------
