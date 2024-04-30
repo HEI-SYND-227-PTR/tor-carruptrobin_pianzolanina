f52fe3e3fdf9f65ea7ec4a77aa9f30d2bb49e00c
Robin Carrupt <robincarrupt@gmail.com>
2024-04-30 10:37:26 +0200

    Added comments

--- mac_receiver.c
+++ mac_receiver.c
@@ -1,3 +1,8 @@
+/**
+ * The `MacReceiver` function processes incoming messages from a queue, performs various checks and
+ * operations based on the message content, and forwards the messages to different queues accordingly.
+ */
+
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
@@ -8,17 +13,22 @@ void MacReceiver(void *argument)
 	osStatus_t retCode;
 	for (;;) // loop until doomsday
 	{
+		// Get the message in the MacR Queue
+		// If empty, wait for a new message
 		retCode = osMessageQueueGet(
 			queue_macR_id,
 			&queueMsg,
 			NULL,
 			osWaitForever);
 		CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);
-
+		
+		// Cast the pointer to a uint8
 		uint8_t *dataPtr = (uint8_t *)queueMsg.anyPtr;
 
 		if(queueMsg.type == FROM_PHY)
 		{
+			// If the message in the queue is a Token
+			// -> Send to the MacSender as is
 			if(dataPtr[0] == TOKEN_TAG)
 			{
 				queueMsg.type = TOKEN; 
@@ -29,6 +39,8 @@ void MacReceiver(void *argument)
 					osWaitForever);
 				CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);
 			}
+			//  Not a token but we are not connected
+			// -> Send directly to the physical sender
 			else if(gTokenInterface.connected == false)
 			{
 				queueMsg.type = TO_PHY;
@@ -41,18 +53,23 @@ void MacReceiver(void *argument)
 			}
 			else // Not a token
 			{
+				// Extract the source and destination addresses and SAPIs
 				uint8_t srcAddr = (dataPtr[0] >> 3);
 				uint8_t destAddr = (dataPtr[1] >> 3);
 				uint8_t srcSapi = (dataPtr[0] & 0b111);
 				uint8_t destSapi = (dataPtr[1] & 0b111);
 				
-				if(destSapi == CHAT_SAPI || destSapi == TIME_SAPI)
+				// If the SAPI of the destination is the same as our SAPI
+				if(((1 << destSapi) == (gTokenInterface.station_list[gTokenInterface.myAddress] & (1 << CHAT_SAPI)))
+					|| ((1 << destSapi) == (gTokenInterface.station_list[gTokenInterface.myAddress] & (1 << TIME_SAPI))))
 				{
+					// If the destination is us
 					if(destAddr == gTokenInterface.myAddress || destAddr == 0xF)
 					{
 						bool read = true;
 						bool ack = false;
 
+						// If the destination is not broadcast
 						if(destAddr != 0xF)
 						{
 							// CRC Compute
@@ -67,14 +84,19 @@ void MacReceiver(void *argument)
 							}
 							dataPtr[3 + dataPtr[2]] = (dataPtr[3 + dataPtr[2]] & 0b11111100) + (ack + (read << 1));
 						} 
-						else
+						else // In broadcast, the ack is always true
 						{
 							ack = true;
 						}
+
+						// We allocate memory for the DataInd and copy the message
 						uint8_t *msg = osMemoryPoolAlloc(memPool, osWaitForever);
 						memcpy(msg,&dataPtr[3],dataPtr[2]);
 						msg[dataPtr[2]] = '\0';
-						if(srcAddr != gTokenInterface.myAddress) //dest is us but not src
+
+						// If the the destination is us but we are not the source
+						// -> Send the message to the physical sender
+						if(srcAddr != gTokenInterface.myAddress) 
 						{
 							queueMsg.type = TO_PHY;
 							retCode = osMessageQueuePut(
@@ -84,7 +106,9 @@ void MacReceiver(void *argument)
 								osWaitForever);
 							CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);	
 						}
-						else if(srcAddr == gTokenInterface.myAddress) //Broadcast and src us
+						// If bothe the destination and the source are us
+						// -> Send the message to the MacSender as a Databack
+						else if(srcAddr == gTokenInterface.myAddress)
 						{
 							queueMsg.type = DATABACK;
 							queueMsg.addr = srcAddr;
@@ -97,13 +121,15 @@ void MacReceiver(void *argument)
 							CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);
 						}
 						
+						// we prepare the DataInd message
 						queueMsg.type = DATA_IND;
 						queueMsg.anyPtr = msg;
 						queueMsg.addr = srcAddr;
 						queueMsg.sapi = srcSapi;
-						
 						if(ack == true)
 						{
+							// If the message has a correct CRC
+							// -> We send Data_IND to the appropriate queue
 							if(destSapi == CHAT_SAPI)
 							{
 								retCode = osMessageQueuePut(
@@ -123,13 +149,15 @@ void MacReceiver(void *argument)
 								CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);							
 							}
 						}
-						else // CRC has problem
+						else // If the CRC has a problem, free the memory
 						{
 							retCode = osMemoryPoolFree(memPool,queueMsg.anyPtr);
 							CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);
 						}
 					}
-					else if(srcAddr == gTokenInterface.myAddress) // Send Databack
+					// If i am the source BUT i am not the destination
+					// -> Send a databack to the MacSender
+					else if(srcAddr == gTokenInterface.myAddress)
 					{
 						queueMsg.type = DATABACK;
 						queueMsg.addr = srcAddr;
@@ -142,6 +170,8 @@ void MacReceiver(void *argument)
 						CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);					
 					}
 				}
+				// If the destination SAPI is not the same as our SAPI
+				// -> Send the message to the physical sender
 				else
 				{
 						queueMsg.type = TO_PHY;
